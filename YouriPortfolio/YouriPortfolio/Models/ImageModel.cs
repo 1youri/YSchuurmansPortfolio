6f8bf5899d80c976b7353b47583963491213a13a
Youri Schuurmans <yourish@live.nl>
2017-04-05 15:44:27 +0200

    imagerepo

---
+++ YouriPortfolio/YouriPortfolio/Models/ImageModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YouriPortfolio.Models
+{
+    public class ImageModel
+    {
+    }
+}
--- YouriPortfolio/YouriPortfolio/Repos/ImageRepo.cs
+++ YouriPortfolio/YouriPortfolio/Repos/ImageRepo.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using PCDataDLL;
 
 namespace YouriPortfolio.Repos
 {
     public class ImageRepo
     {
+        public static bool InsertImage(int contentID, )
+        {
+            string sql = "UPDATE CONTENT SET TITLE=?, ShortContentBlock=?, ContentBlock=?, HeaderIMG=? WHERE ID=?";
+            Dictionary<string, object> parameters = new Dictionary<string, object>()
+            {
+                //{"Title", toEdit.Title},
+                //{"ShortContentBlock", toEdit.ShortContent },
+                //{"ContentBlock", toEdit.ContentText },
+                //{"HeaderImg", toEdit.HeaderIMG },
+                //{"id", toEdit.ID}
+            };
+            var result = DB.PFDB.UpdateQuery(sql, parameters);
+
+            return result;
+        }
     }
 }
